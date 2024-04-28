cb64cdbb4b41c93c6d1012aef6009b94e7124944
jase863 <jason.walker8630@gmail.com>
2024-04-27 21:39:32 -0600

    successfully wrote the methods and added comments

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -39,7 +39,22 @@ public static class ArraysTester {
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return new double[0]; // replace this return statement with your own
+        // create a new double array with capacity of 'length'
+        double[] multiples = new double[length];
+
+        // create a for loop that starts with int 'i' equal to 0, loops 'length' times, and increases 'i' by one after each iteration
+        for (int i = 0; i < length; i++){
+
+            // take 'number' times ('i' + 1) and store it as a double called 'multipliedNumber' (the 'i' + 1 ensures that 0 doesn't end up as one of the multiples)
+            double multipliedNumber = number * (i+1);
+
+            // set the index 'i' in the array 'multiples' equal to 'mutlipliedNumber'
+            multiples[i] = multipliedNumber;
+        }
+
+
+        //return the array 'multiples'
+        return multiples; // replace this return statement with your own
     }
     
     /// <summary>
@@ -56,6 +71,16 @@ public static class ArraysTester {
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // create a new list (int) called 'temporaryList' and use it to store the range of data that will be removed.
+        // set it equal to the GetRange() method, with (length of 'data' - 'amount') and 'amount' as arguments.
+        List<int> temporaryList = data.GetRange(data.Count - amount, amount);
+
+        // remove the specified range using the RemoveRange() method on 'data', with (length of 'data' - 'amount') and 'amount' as arguments
+        data.RemoveRange(data.Count - amount, amount);
+
+        // insert the 'temporaryList' at index 0. This will move all subsequent data to the right.
+        data.InsertRange(0, temporaryList);
 
     }
 }
