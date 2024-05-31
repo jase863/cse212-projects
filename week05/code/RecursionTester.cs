a61b94b7934d2a8533fe54dafd16eda4ed7eb389
jase863 <jason.walker8630@gmail.com>
2024-05-30 22:31:30 -0600

    finished up to problem 3

--- week05/code/RecursionTester.cs
+++ week05/code/RecursionTester.cs
@@ -67,7 +67,7 @@ public static class RecursionTester {
         Console.WriteLine(CountWaysToClimb(20)); // 121415
         // Uncomment out the test below after implementing memoization.  It won't work without it.
         // TODO Problem 3
-        // Console.WriteLine(CountWaysToClimb(100));  // 180396380815100901214157639
+        Console.WriteLine(CountWaysToClimb(100));  // 180396380815100901214157639
 
         // Sample Test Cases (may not be comprehensive) 
         Console.WriteLine("\n=========== PROBLEM 4 TESTS ===========");
@@ -260,6 +260,11 @@ public static class RecursionTester {
     /// until the memoization is implemented.
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null) {
+        
+        if (remember == null){
+            remember = new Dictionary<int, decimal>();
+        }
+
         // Base Cases
         if (s == 0)
             return 0;
@@ -271,7 +276,14 @@ public static class RecursionTester {
             return 4;
 
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+
+        if (remember.ContainsKey(s)){
+            return remember[s];
+        }
+
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+
+        remember[s] = ways;
         return ways;
     }
 
@@ -292,7 +304,11 @@ public static class RecursionTester {
         // TODO Start Problem 4
         
         if (!pattern.Contains(char.Parse("*"))){
+            Console.WriteLine(pattern);
+        }
 
+        else{
+            
         }
     }
 
